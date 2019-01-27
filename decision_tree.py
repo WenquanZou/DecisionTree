242f7c59773782966617098405a9ce5798c4e390
Wenquan Zou <wz1816@ic.ac.uk>
2019-01-27 21:22:19 +0000

    `[wz1816] Reconstruct node in decision tree`

--- decision_tree.py
+++ decision_tree.py
@@ -21,35 +21,36 @@ def split_dataset(train_dataset, attr, value):
     return left, right
 
 
-def construct_node(train_dataset, is_leaf=True, attr=None, value=None):
-    # Split the dataset by the value of given attr
-    left, right = split_dataset(train_dataset, attr, value)
-    # Node structure: {attrs, left_subset, right_subset, label}, left and right subset are still the input dataset
-    # format
-    if is_leaf:
-        return {"attrs": train_dataset[0]['attrs'], "left": train_dataset[1:],
-                "right": [], "label": train_dataset[0]['label']}
-    else:
-        if attr is None:
-            raise Exception("Attribute not define")
-        if value is None:
-            raise Exception("Value not define")
-        return {"attrs": train_dataset[0]['attrs'], "left": left, "right": right, "label": train_dataset[0]['label']}
-
-
 def decision_tree_learning(train_dataset, depth):
     # train_dataset: [ {attrs:list, label:value}, ...]
     if check_label(train_dataset):
-        # Base case: when all the label of dataset are the same then return a left node of the tree and the
-        # corresponding label
-        node = construct_node(train_dataset)
-        return node, train_dataset[0]['label']
+        # Base case: when all the label of dataset are the same then return a leaf node of the tree and the
+        # corresponding depth
+        node = {"attr": None, "value": None,
+                "left": None, "right": None, "is_leaf": True, "label": train_dataset[0]["label"]}
+        return node, depth
     else:
-        # TODO: Finish the recursive structure
         # Find best split method
         split_attr, split_value = find_split(train_dataset)
+        left_dataset, right_dataset = split_dataset(train_dataset, split_attr, split_value)
+        left, l_depth = decision_tree_learning(left_dataset, depth + 1)
+        right, r_depth = decision_tree_learning(right_dataset, depth + 1)
+        node = {"attr": split_attr, "value": split_value,
+                "left": left, "right": right, "is_leaf": False, "label": None}
+        return node, max(l_depth, r_depth)
+
+
+def predict(trained_node, data):
+    # TODO
+    # trained_node:: decision tree, data::{attrs:list, label:value}
+    pass
+
+
+def evaluate(trained_node, test_dataset, k=10):
+    # TODO: 10 fold cross validation
+    pass
 
 
 file = np.loadtxt('co395-cbc-dt/wifi_db/clean_dataset.txt')
-train_dataset = [{"attrs": line[:-2], "label":line[-1]} for line in file]
+train_dataset = [{"attrs": line[:-2], "label": line[-1]} for line in file]
 print(train_dataset)

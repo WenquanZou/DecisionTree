3dcb701fa7cc36bf4fe14ee0c4c27302cff8b9aa
WenquanZou <wz1816@ic.ac.uk>
2019-02-02 07:47:35 +0000

    '[wz1816]Add comments to entropy and evaluate functions'

--- decision_tree.py
+++ decision_tree.py
@@ -18,27 +18,35 @@ def find_entropy_attr(dataset, target_attr):
     for data in dataset:
         unique_value.add(data["attrs"][target_attr])
     for v in unique_value:
+        # Every distinct value with its probability discretely
         count = value_set.count(v)
         prob = count / len(value_set)
+        # Compute entropy for the value
         entropy = entropy + -prob * np.log2(prob)
     return entropy
 
 
-def find_entropy_value(entropy_total, dataset, target):
+def find_information_gain(entropy_total, dataset, target):
+    # Find the entropy of an attribute in every possible value to split and return the best split value
+    #  from this attribute
     unique_value = set()
     best_split_value = 0
     max_gain = 0
     for data in dataset:
+        # Distinct values
         unique_value.add(data["attrs"][target])
     for v in unique_value:
+        # Generate subset for greater and smaller
         greater_dataset = list(filter(lambda x: x["attrs"][target] >= v, dataset))
         smaller_dataset = list(filter(lambda x: x["attrs"][target] < v, dataset))
+        # Compute each entropy for both subset
         g_entropy = find_entropy_attr(greater_dataset, target)
         s_entropy = find_entropy_attr(smaller_dataset, target)
         remainder = len(greater_dataset) / len(dataset) * g_entropy + len(smaller_dataset) / len(dataset) * s_entropy
         gain = entropy_total - remainder
         if gain > max_gain:
             max_gain = gain
+            # Reassign split value if greater gain is found
             best_split_value = v
     return best_split_value
 
@@ -51,7 +59,7 @@ def find_split(train_dataset):
         if max_entropy < cur_entropy:
             max_entropy = cur_entropy
             split_attr = i
-    return split_attr, find_entropy_value(max_entropy, train_dataset, split_attr)
+    return split_attr, find_information_gain(max_entropy, train_dataset, split_attr)
 
 
 def split_dataset(train_dataset, attr, value):
@@ -66,9 +74,8 @@ def decision_tree_learning(train_dataset, depth):
     if check_label(train_dataset):
         # Base case: when all the label of dataset are the same then return a leaf node of the tree and the
         # corresponding depth
-        node = {"attr": None, "value": None,
-                "left": None, "right": None, "is_leaf": True, "label": train_dataset[0]["label"]}
-        return node, depth
+        return {"attr": None, "value": None,
+                "left": None, "right": None, "is_leaf": True, "label": train_dataset[0]["label"]}, depth
     else:
         # Find best split method
         split_attr, split_value = find_split(train_dataset)
@@ -78,9 +85,8 @@ def decision_tree_learning(train_dataset, depth):
         left, l_depth = decision_tree_learning(left_dataset, depth + 1)
         right, r_depth = decision_tree_learning(right_dataset, depth + 1)
         # Construct root node
-        node = {"attr": split_attr, "value": split_value,
-                "left": left, "right": right, "is_leaf": False, "label": None}
-        return node, max(l_depth, r_depth)
+        return {"attr": split_attr, "value": split_value,
+                "left": left, "right": right, "is_leaf": False, "label": None}, max(l_depth, r_depth)
 
 
 def predict(trained_node, data):
@@ -94,12 +100,13 @@ def predict(trained_node, data):
         right = trained_node["right"]
         return predict(right, data)
 
+
 def cross_fold_split(train_dataset):
     # Split up the dataset and double shuffle
     dataset = list()
     train_dataset_copy = list(train_dataset)
     fold_size = int(len(train_dataset) / 10)
-    for i in range (10):
+    for i in range(10):
         fold_set = list()
         while len(fold_set) < fold_size:
             rand_index = np.random.randint(0, len(train_dataset_copy))
@@ -109,17 +116,21 @@ def cross_fold_split(train_dataset):
 
 
 def c_matrix(actual, predicted):
-    c_matrix = np.zeros((4,4))
+    # Generate confusion matrix
+    c_matrix = np.zeros((4, 4))
     for i in range(len(actual)):
         c_matrix[int(actual[i]) - 1][int(predicted[i]) - 1] += 1
     return c_matrix
 
+
 def calc_eval(c_matrix):
+    # Keep track on precision and recall of every label to sum
     precision_sum = 0
     recall_sum = 0
     rate_sum = 0
     total = 0
     for i in range(4):
+        # One label precision and recall
         precision_denom = 0
         recall_denom = 0
         for j in range(4):
@@ -130,30 +141,35 @@ def calc_eval(c_matrix):
         recall_sum += (c_matrix[i][i] / recall_denom)
         rate_sum += c_matrix[i][i]
 
+    # Find average precision and recall then find the f1 score
     precision = precision_sum / 4
     recall = recall_sum / 4
 
+    # TODO: Require precision recall f1 score for every label
     f1_data = 2 * precision * recall / (precision + recall)
     rate = rate_sum / total
     return precision, recall, f1_data, rate
 
 
 def cross_validation(dataset):
+    # Split dataset into 10 folds
     folds = cross_fold_split(dataset)
     for fold in folds:
-        train_dataset = list(folds)
-        train_dataset.remove(fold)
-        train_dataset = sum(train_dataset, [])
-        test_dataset = list(fold)
-        dtree, _ = decision_tree_learning(train_dataset, 0)
+        # Choose every fold as test set in random order
+        train_folds = list(folds)
+        train_folds.remove(fold)
+        train_folds = sum(train_folds, [])
+        test_fold = list(fold)
+        # Training process
+        dtree, _ = decision_tree_learning(train_folds, 0)
         predicted_labels = list()
         actual_labels = list()
-        for data in test_dataset:
+        # Get metrics for every testing fold
+        for data in test_fold:
             predicted_labels.append(predict(dtree, data["attrs"]))
             actual_labels.append(data["label"])
             print(predicted_labels, actual_labels)
-        print(c_matrix(actual_labels, predicted_labels))
-        print(calc_eval(c_matrix(actual_labels,predicted_labels)))
+        print(calc_eval(c_matrix(actual_labels, predicted_labels)))
     pass
 
 
@@ -162,8 +178,8 @@ def evaluate(test_dataset, trained_tree):
 
     pass
 
+
 file = np.loadtxt('co395-cbc-dt/wifi_db/clean_dataset.txt')
 train_dataset = [{"attrs": list(line[:-1]), "label": line[-1]} for line in file]
 node, _ = decision_tree_learning(train_dataset, 0)
-print(predict(node, [-52,-55,-52,-43,-61,-86,-83]))
 print(cross_validation(train_dataset))
